00f11830f3165e5585a054ee4335d3c44627644e
LEELA-SRI <leelasric@gmail.com>
2023-06-15 22:28:42 +0530

    update

--- Trains/app.py
+++ Trains/app.py
@@ -1,13 +1,11 @@
-from flask import Flask,request,jsonify
+from flask import Flask,request,jsonify,render_template
 import requests
+import datetime
 
 app = Flask(__name__)
 
-@app.route('/trains', methods=['GET'])
-def get_trains():
-
-    url = 'http://104.211.219.98/train/auth'
-    credentials = {
+url = 'http://104.211.219.98/train/auth'
+credentials = {
     "companyName": "Train Central",
     "clientID": "94edbb0a-ecc7-4e0d-a77e-edd4d786429b",
     "clientSecret": "lwiuIbjJbgkIxMkI",
@@ -15,6 +13,34 @@ def get_trains():
     "ownerEmail": "leelasric@gmail.com",
     "rollNo": "40731022"
     }
+
+def sort_key(train):
+    current_time = datetime.datetime.now()
+    departure_time = datetime.datetime(
+        year=current_time.year,
+        month=current_time.month,
+        day=current_time.day,
+        hour=train["departureTime"]["Hours"],
+        minute=train["departureTime"]["Minutes"],
+        second=train["departureTime"]["Seconds"],
+    )
+    if departure_time <= current_time + datetime.timedelta(minutes=30):
+        return float("inf")
+    return (
+        train["trainNumber"],
+        -train["seatsAvailable"]["sleeper"],
+        -train["seatsAvailable"]["AC"],
+        -train["departureTime"]["Hours"],
+        -train["departureTime"]["Minutes"],
+        -train["departureTime"]["Seconds"],
+        train["delayedBy"]
+    )  
+
+
+@app.route('/trains', methods=['GET'])
+def get_trains():
+
+    
     post_req = requests.post(url, json=credentials)
     post_req_response =post_req.json()
     token =post_req_response["access_token"]
@@ -24,7 +50,21 @@ def get_trains():
     })
 
     trains_data = get_trains.json()
-    return 'hi'
+    sorted_trains_data = sorted(trains_data, key=sort_key)
+    return render_template('trains.html',sorted_trains_data=sorted_trains_data)
+
+@app.route('/trains/<int:number>')
+def get_train_info(number):
+    post_req = requests.post(url, json=credentials)
+    post_req_response =post_req.json()
+    token =post_req_response["access_token"]
+
+    get_train_details = requests.get(f"http://104.211.219.98:80/train/trains/{number}",headers={
+        'Authorization': f'Bearer {token}'
+    })
+    train_data = get_train_details.json()
+    print(train_data)
+    return render_template("train_det.html",train=train_data)
 
 
 if __name__ == '__main__':
